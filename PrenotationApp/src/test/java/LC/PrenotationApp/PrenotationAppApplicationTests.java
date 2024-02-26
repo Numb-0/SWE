b6d329ebc1e4553555060734c24c4c8eae7abe63
Numb-0 <cosimofalla@gmail.com>
2024-02-26 15:02:03 +0100

    added spring

---
+++ PrenotationApp/src/test/java/LC/PrenotationApp/PrenotationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package LC.PrenotationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
