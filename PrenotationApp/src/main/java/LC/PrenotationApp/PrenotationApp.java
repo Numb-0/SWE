18302531fbdd986e8788ad36ba196c9090e0158e
Numb-0 <cosimofalla@gmail.com>
2024-02-26 16:10:13 +0100

    added index.html

--- PrenotationApp/src/main/java/LC/PrenotationApp/PrenotationApp.java
+++ PrenotationApp/src/main/java/LC/PrenotationApp/PrenotationApp.java
@@ -3,17 +3,16 @@ package LC.PrenotationApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
+// All resources in static get automatic-loaded .js .html .css
 @SpringBootApplication
-@RestController
-public class PrenotationAppApplication {
+public class PrenotationApp {
 	public static void main(String[] args) {
-		SpringApplication.run(PrenotationAppApplication.class, args);
-	}
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		SpringApplication.run(PrenotationApp.class, args);
 	}
 }
