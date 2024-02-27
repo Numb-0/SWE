f6dada019cee5800f3e3e74c99b47647e12408b8
Numb-0 <cosimofalla@gmail.com>
2024-02-27 17:22:49 +0100

    tested JDBC

--- PrenotationApp/src/main/java/LC/PrenotationApp/PrenotationApp.java
+++ PrenotationApp/src/main/java/LC/PrenotationApp/PrenotationApp.java
@@ -1,5 +1,6 @@
 package LC.PrenotationApp;
 
+import java.sql.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
