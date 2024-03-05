74515168c8c0b4fbda805eb720f14064c9696086
Numb-0 <cosimofalla@gmail.com>
2024-03-05 13:53:15 +0100

    Html pages and web controller

--- PrenotationApp/src/main/java/LC/PrenotationApp/MainController.java
+++ PrenotationApp/src/main/java/LC/PrenotationApp/MainController.java
@@ -27,6 +27,7 @@ public class MainController {
         return "Saved";
     }
 
+    
     @GetMapping(path="/all")
     public @ResponseBody Iterable<User> getAllUsers() {
         // This returns a JSON or XML with the users
