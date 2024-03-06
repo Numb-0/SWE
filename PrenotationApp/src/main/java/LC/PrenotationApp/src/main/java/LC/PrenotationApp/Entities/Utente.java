8aba68109a6f201b4f5584bb72fef96b7b0ea8a2
Numb-0 <cosimofalla@gmail.com>
2024-03-06 14:25:38 +0100

    yesterday work

--- PrenotationApp/src/main/java/LC/PrenotationApp/src/main/java/LC/PrenotationApp/Entities/Utente.java
+++ PrenotationApp/src/main/java/LC/PrenotationApp/src/main/java/LC/PrenotationApp/Entities/Utente.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
-@Table(name= "Utenti")
+@Table(name="Utenti")
 public class Utente {
     public enum Role {
         user,
@@ -16,18 +16,26 @@ public class Utente {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    private String userName;
+    private String username;
     private String password;
     private String email;
     private Role role;
 
-    protected Utente() {}
+    public Utente() {}
 
-    public Utente(String userName, String password, String email) {
-        this.userName = userName;
+    public Utente(String username, String password, String email) {
+        this.username = username;
         this.password = password;
         this.email = email;
         this.role = Role.user;
+    }
+
+    void setUsername(String username) {
+        this.username = username;
+    }
+
+    void setPassword(String password) {
+        this.password = password;
     }
     @OneToMany(mappedBy = "utente", cascade = CascadeType.ALL)
     private Set<Recensione> recensioni;
---
+++ PrenotationApp/src/main/java/LC/PrenotationApp/src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById("login_link_button").onclick = () => {
+    location.href = "login.html";
+}
+
+document.getElementById("register_link_button").onclick = () => {
+    location.href = "register.html";
+}
